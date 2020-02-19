f8b10f755d18b8705099c3fa29288e1aa6186b2f
h11 <nirisix@gmail.com>
2020-02-19 14:16:30 +0800

    add more func

--- bindings/bindings.ml
+++ bindings/bindings.ml
@@ -3,30 +3,11 @@ open Ctypes
 module Make (F : Cstubs.FOREIGN) = struct
   open F
 
-  (*
-   * JSValue
-   * JS_DupValue
-   * JS_FreeValue
-   * JS_NewCFunction
-   * JS_SetPropertyFunctionList
-   * JS_EXCEPTION
-   * JS_GetException
-   * JS_Eval
-   * js_std_eval_binary
-   * JS_NewClassID
-   * JS_NewClass
-   * JS_NewObjectClass
-   * JS_GetOpaque
-   * JS_SetOpaque
-   * JS_SetClassProto
-   * JS_NewRuntime2
-   * JS_SetMaxStackSize
-   * JS_SetInterruptHandler
-   *)
-  (* typedef struct JSObject JSObject; *)
-  (* typedef struct JSClass JSClass; *)
-
   (* --- *)
+
+  type js_runtime
+
+  let js_runtime : js_runtime structure typ = structure "JSRuntime"
 
   type js_context
 
@@ -34,15 +15,7 @@ module Make (F : Cstubs.FOREIGN) = struct
 
   (* --- *)
 
-  type js_runtime
-
-  let js_runtime : js_runtime structure typ = structure "JSRuntime"
-
-  (* --- *)
-
   let js_class_id = uint32_t
-
-  let js_atom = uint32_t
 
   let js_bool = int
 
@@ -73,6 +46,24 @@ module Make (F : Cstubs.FOREIGN) = struct
   let () = seal js_value
 
   let js_value_const = js_value
+
+  (* --- *)
+
+  (* typedef JSValue JSCFunction(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv); *)
+  (* type js_c_function *)
+  (* let js_c_function = *)
+  (*   funptr *)
+  (*     Ctypes.( *)
+  (*       ptr js_context *)
+  (*       @-> js_value_const *)
+  (*       @-> int *)
+  (*       @-> ptr js_value_const *)
+  (*       @-> returning js_value) *)
+
+  type js_c_function_list_entry
+
+  let js_c_function_list_entry : js_c_function_list_entry structure typ =
+    structure "JSCFunctionListEntry"
 
   (* --- *)
 
@@ -114,6 +105,203 @@ module Make (F : Cstubs.FOREIGN) = struct
     (* void JS_SetMaxStackSize(JSContext *ctx, size_t stack_size); *)
     foreign "JS_SetMaxStackSize" (ptr js_context @-> size_t @-> returning void)
 
+  (* --- *)
+
+  (* let js_new_c_function = *)
+  (*   (* JSValue JS_NewCFunction(JSContext *ctx, JSCFunction *func, const char *name, int length) *) *)
+  (*   foreign *)
+  (*     "JS_NewCFunction" *)
+  (*     (ptr js_context *)
+  (*     @-> ptr js_c_function *)
+  (*     @-> string *)
+  (*     @-> int *)
+  (*     @-> returning js_value *)
+  (*     ) *)
+
+  let js_set_property_function_list =
+    (* void JS_SetPropertyFunctionList(JSContext *ctx, JSValueConst obj, const JSCFunctionListEntry *tab, int len); *)
+    foreign
+      "JS_SetPropertyFunctionList"
+      (ptr js_context
+      @-> js_value_const
+      @-> ptr js_c_function_list_entry
+      @-> int
+      @-> returning void
+      )
+
+  (* --- *)
+
+  let js_free_value =
+    (* void JS_FreeValue(JSContext *ctx, JSValue v) *)
+    foreign "JS_FreeValue" (ptr js_context @-> js_value @-> returning void)
+
+  let js_free_value_rt =
+    (* void JS_FreeValueRT(JSRuntime *rt, JSValue v) *)
+    foreign "JS_FreeValueRT" (ptr js_runtime @-> js_value @-> returning void)
+
+  let js_dup_value =
+    (* JSValue JS_DupValue(JSContext *ctx, JSValueConst v) *)
+    foreign
+      "JS_DupValue"
+      (ptr js_context @-> js_value_const @-> returning js_value)
+
+  let js_dup_value_rt =
+    (* JSValue JS_DupValueRT(JSRuntime *rt, JSValueConst v) *)
+    foreign
+      "JS_DupValueRT"
+      (ptr js_runtime @-> js_value_const @-> returning js_value)
+
+  (* --- *)
+
+  (*
+  JS_BOOL JS_IsUninitialized(JSValueConst v)
+  JS_BOOL JS_IsError(JSContext *ctx, JSValueConst val);
+  JS_BOOL JS_IsException(JSValueConst v)
+  JS_BOOL JS_IsNull(JSValueConst v)
+  JS_BOOL JS_IsUndefined(JSValueConst v)
+  JS_BOOL JS_IsBool(JSValueConst v)
+  JS_BOOL JS_IsNumber(JSValueConst v)
+  JS_BOOL JS_IsString(JSValueConst v)
+  JS_BOOL JS_IsSymbol(JSValueConst v)
+  int JS_IsArray(JSContext *ctx, JSValueConst val);
+  JS_BOOL JS_IsObject(JSValueConst v)
+  JS_BOOL JS_IsFunction(JSContext* ctx, JSValueConst val);
+  JS_BOOL JS_IsConstructor(JSContext* ctx, JSValueConst val);
+  int JS_IsInstanceOf(JSContext *ctx, JSValueConst val, JSValueConst obj);
+  JS_BOOL JS_IsBigInt(JSContext *ctx, JSValueConst v)
+  JS_BOOL JS_IsBigFloat(JSValueConst v)
+  JS_BOOL JS_IsBigDecimal(JSValueConst v)
+  *)
+  let js_is_uninitialized =
+    foreign "JS_IsUninitialized" (js_value_const @-> returning js_bool)
+
+  let js_is_error =
+    foreign
+      "JS_IsError"
+      (ptr js_context @-> js_value_const @-> returning js_bool)
+
+  let js_is_exception =
+    foreign "JS_IsException" (js_value_const @-> returning js_bool)
+
+  let js_is_null = foreign "JS_IsNull" (js_value_const @-> returning js_bool)
+
+  let js_is_undefined =
+    foreign "JS_IsUndefined" (js_value_const @-> returning js_bool)
+
+  let js_is_bool = foreign "JS_IsBool" (js_value_const @-> returning js_bool)
+
+  let js_is_number = foreign "JS_IsNumber" (js_value_const @-> returning js_bool)
+
+  let js_is_string = foreign "JS_IsString" (js_value_const @-> returning js_bool)
+
+  let js_is_symbol = foreign "JS_IsSymbol" (js_value_const @-> returning js_bool)
+
+  let js_is_array =
+    foreign
+      "JS_IsArray"
+      (ptr js_context @-> js_value_const @-> returning js_bool)
+
+  let js_is_object = foreign "JS_IsObject" (js_value_const @-> returning js_bool)
+
+  let js_is_function =
+    foreign
+      "JS_IsFunction"
+      (ptr js_context @-> js_value_const @-> returning js_bool)
+
+  let js_is_constructor =
+    foreign
+      "JS_IsConstructor"
+      (ptr js_context @-> js_value_const @-> returning js_bool)
+
+  let js_is_instance_of =
+    foreign
+      "JS_IsInstanceOf"
+      (ptr js_context
+      @-> js_value_const
+      @-> js_value_const
+      @-> returning js_bool
+      )
+
+  let js_is_big_int =
+    foreign
+      "JS_IsBigInt"
+      (ptr js_context @-> js_value_const @-> returning js_bool)
+
+  let js_is_big_float =
+    foreign "JS_IsBigFloat" (js_value_const @-> returning js_bool)
+
+  let js_is_big_decimal =
+    foreign "JS_IsBigDecimal" (js_value_const @-> returning js_bool)
+
+  (* --- *)
+
+  (*
+  int JS_ToBool(JSContext *ctx, JSValueConst val); /* return -1 for JS_EXCEPTION */
+  int JS_ToInt32(JSContext *ctx, int32_t *pres, JSValueConst val);
+  int JS_ToUint32(JSContext *ctx, uint32_t *pres, JSValueConst val)
+  int JS_ToInt64(JSContext *ctx, int64_t *pres, JSValueConst val);
+  int JS_ToFloat64(JSContext *ctx, double *pres, JSValueConst val);
+  int JS_ToBigInt64(JSContext *ctx, int64_t *pres, JSValueConst val);
+  JSValue JS_ToString(JSContext *ctx, JSValueConst val);
+  *)
+
+  let js_to_bool =
+    foreign "JS_ToBool" (ptr js_context @-> js_value_const @-> returning int)
+
+  let js_to_int32 =
+    foreign
+      "JS_ToInt32"
+      (ptr js_context @-> ptr int32_t @-> js_value_const @-> returning int)
+
+  let js_to_uint32 =
+    foreign
+      "JS_ToUint32"
+      (ptr js_context @-> ptr uint32_t @-> js_value_const @-> returning int)
+
+  let js_to_int64 =
+    foreign
+      "JS_ToInt64"
+      (ptr js_context @-> ptr int64_t @-> js_value_const @-> returning int)
+
+  let js_to_float64 =
+    foreign
+      "JS_ToFloat64"
+      (ptr js_context @-> ptr double @-> js_value_const @-> returning int)
+
+  let js_to_big_int64 =
+    foreign
+      "JS_ToBigInt64"
+      (ptr js_context @-> ptr int64_t @-> js_value_const @-> returning int)
+
+  let js_to_string =
+    foreign
+      "JS_ToString"
+      (ptr js_context @-> js_value_const @-> returning js_value)
+
+  (* --- *)
+
+  let js_to_c_string =
+    foreign
+      (* const char *JS_ToCString(JSContext *ctx, JSValueConst val1) *)
+      "JS_ToCString"
+      (ptr js_context @-> js_value_const @-> returning string)
+
+  let js_free_c_string =
+    (* void JS_FreeCString(JSContext *ctx, const char *ptr); *)
+    foreign "JS_FreeCString" (ptr js_context @-> string @-> returning void)
+
+  (* --- *)
+
+  let js_get_exception =
+    (* JSValue JS_GetException(JSContext *ctx); *)
+    foreign "JS_GetException" (ptr js_context @-> returning js_value)
+
+  let js_throw =
+    (* JSValue JS_Throw(JSContext *ctx, JSValue obj); *)
+    foreign "JS_Throw" (ptr js_context @-> js_value @-> returning js_value)
+
+  (* --- *)
+
   let js_set_class_proto =
     (* void JS_SetClassProto(JSContext *ctx, JSClassID class_id, JSValue obj); *)
     foreign
@@ -125,6 +313,40 @@ module Make (F : Cstubs.FOREIGN) = struct
     foreign
       "JS_GetClassProto"
       (ptr js_context @-> js_class_id @-> returning js_value)
+
+  let js_new_class_id =
+    (* JSClassID JS_NewClassID(JSClassID *pclass_id); *)
+    foreign "JS_NewClassID" (ptr js_class_id @-> returning js_class_id)
+
+  let js_new_object_proto_class =
+    (* JSValue JS_NewObjectProtoClass(JSContext *ctx, JSValueConst proto, JSClassID class_id); *)
+    foreign
+      "JS_NewObjectProtoClass"
+      (ptr js_context @-> js_value_const @-> js_class_id @-> returning js_value)
+
+  let js_new_object_class =
+    (* JSValue JS_NewObjectClass(JSContext *ctx, int class_id); *)
+    foreign "JS_NewObjectClass" (ptr js_context @-> int @-> returning js_value)
+
+  let js_new_object_proto =
+    (* JSValue JS_NewObjectProto(JSContext *ctx, JSValueConst proto); *)
+    foreign
+      "JS_NewObjectProto"
+      (ptr js_context @-> js_value_const @-> returning js_value)
+
+  let js_new_object =
+    (* JSValue JS_NewObject(JSContext *ctx); *)
+    foreign "JS_NewObject" (ptr js_context @-> returning js_value)
+
+  let js_set_opaque =
+    (* void JS_SetOpaque(JSValue obj, void *opaque); *)
+    foreign "JS_SetOpaque" (js_value @-> ptr void @-> returning void)
+
+  let js_get_opaque =
+    (* void *JS_GetOpaque(JSValueConst obj, JSClassID class_id); *)
+    foreign
+      "JS_GetOpaque"
+      (js_value_const @-> js_class_id @-> returning (ptr void))
 
   (* --- *)
 
@@ -139,4 +361,9 @@ module Make (F : Cstubs.FOREIGN) = struct
       @-> int
       @-> returning js_value
       )
+
+  (* --- *)
+
+  (* typedef int JSInterruptHandler(JSRuntime *rt, void *opaque); *)
+  (* void JS_SetInterruptHandler(JSRuntime *rt, JSInterruptHandler *cb, void *opaque); *)
 end
--- src/quickjs.ml
+++ src/quickjs.ml
@@ -1,27 +1,66 @@
 module C = Bindings.Make (Stubs)
 
-let to_sizet = Unsigned.Size_t.of_int
+module Q : sig
+  type runtime
+  (** JSRuntime represents a Javascript runtime corresponding to an object heap.
+    Several runtimes can exist at the same time but they cannot exchange objects.
+    Inside a given runtime, no multi-threading is supported.
+    *)
 
-type runtime = C.js_runtime Ctypes.structure Ctypes.ptr
+  val new_runtime : unit -> runtime
 
-let new_runtime () : runtime =
-  let rt = C.js_new_runtime () in
-  let () = Gc.finalise (fun rt -> C.js_free_runtime rt) rt in
-  rt
+  (* --- *)
 
-type context = {
-  rt: runtime;
-  ctx: C.js_context Ctypes.structure Ctypes.ptr;
-}
+  type context
+  (** JSContext represents a Javascript context (or Realm). Each JSContext has
+    its own global objects and system objects. There can be several JSContexts
+    per JSRuntime and they can share objects, similar to frames of the same
+    origin sharing Javascript objects in a web browser.
+    *)
 
-let new_context (rt : runtime) : context =
-  let ctx = C.js_new_context rt in
-  let () = Gc.finalise (fun c -> C.js_free_context c) ctx in
-  { rt; ctx }
+  val new_context : runtime -> context
 
-type value = C.js_value Ctypes.structure
+  (* --- *)
 
-let eval (ctx : context) (script : string) : value =
-  let len = to_sizet (String.length script) in
-  let r = C.js_eval ctx.ctx script len "" 0 in
-  r
+  type value
+  (** JSValue represents a Javascript value which can be a primitive type or an
+    object.
+    *)
+
+  val eval : context -> string -> (value, string) result
+end = struct
+  type runtime = C.js_runtime Ctypes.structure Ctypes.ptr
+
+  let new_runtime () : runtime =
+    let rt = C.js_new_runtime () in
+    let () = Gc.finalise (fun rt -> C.js_free_runtime rt) rt in
+    rt
+
+  (* --- *)
+
+  type context = {
+    rt: runtime;
+    ctx: C.js_context Ctypes.structure Ctypes.ptr;
+  }
+
+  let new_context (rt : runtime) : context =
+    let ctx = C.js_new_context rt in
+    let () = Gc.finalise (fun c -> C.js_free_context c) ctx in
+    { rt; ctx }
+
+  (* --- *)
+
+  type value = {
+    ctx: context;
+    v: C.js_value Ctypes.structure;
+  }
+
+  (* --- *)
+
+  let eval (ctx : context) (script : string) : (value, string) result =
+    let len = Unsigned.Size_t.of_int (String.length script) in
+    let v = C.js_eval ctx.ctx script len "input.js" 0 in
+    Ok { ctx; v }
+end
+
+include Q
