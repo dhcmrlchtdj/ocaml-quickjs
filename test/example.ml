39e66d5bad11aaa11c2d831c644c05fe7ba76679
h11 <nirisix@gmail.com>
2020-02-19 21:32:09 +0800

    example: add fib

--- test/example.ml
+++ test/example.ml
@@ -1,6 +1,8 @@
 let ( let* ) = Result.bind
 
-let test () =
+let ( let+ ) r f = Result.map f r
+
+let _ =
   let* sum = Quickjs.eval_once "1+1" in
   let typ = Quickjs.Value.is_string sum in
   print_endline (if typ then "sum is string" else "sum isn't string");
@@ -8,7 +10,22 @@ let test () =
   print_endline sums;
   let typ = Quickjs.Value.is_number sum in
   print_endline (if typ then "sum is number" else "sum isn't number");
-  let* sumi = Quickjs.Value.to_int32 sum in
-  Ok (sumi |> Int32.to_string |> print_endline)
+  let+ sumi = Quickjs.Value.to_int32 sum in
+  sumi |> Int32.to_string |> print_endline
 
-let _ = test ()
+let _ =
+  let script =
+    {|
+    const fib = (n) => {
+      if (n <= 1) {
+        return n
+      } else {
+        return fib(n-2) + fib(n-1)
+      }
+    }
+    fib(20)
+  |}
+  in
+  let* r = Quickjs.eval_once script in
+  let+ r = Quickjs.Value.to_string r in
+  print_endline r
