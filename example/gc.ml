f795f686077fd582ce7051ccb6f12689c19c2651
h11 <nirisix@gmail.com>
2020-02-25 19:10:35 +0800

    disable gc stat example

--- example/gc.ml
+++ example/gc.ml
@@ -1,5 +1,6 @@
 let _ =
   let rt = Quickjs.new_runtime () in
   let () = Quickjs.run_gc rt in
-  let stats = Quickjs.(compute_memory_usage rt |> memory_usage_to_string) in
-  print_endline stats
+  (* let stats = Quickjs.(compute_memory_usage rt |> memory_usage_to_string) in *)
+  (* print_endline stats *)
+  ()
